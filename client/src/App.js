622fe2510b36a6116159d0f5b41153e0636f5913
wahyu alfarisi <wahyualfarisi@gmail.com>
2019-04-23 16:46:00 +0700

    initalize redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import {BrowserRouter as Router , Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+
+//static
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+
+
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Route exact={true} path="/" component={ Landing } title={'HEllo'} />
-          <div className="container" >
-            <Route exact path="/register" component={ Register }  />
-            <Route exact path="/login" component={ Login } />
-          </div>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact={true} path="/" component={ Landing } title={'HEllo'} />
+            <div className="container" >
+              <Route exact path="/register" component={ Register }  />
+              <Route exact path="/login" component={ Login } />
+            </div>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
 
@@ -38,13 +39,17 @@ class Register extends Component {
 
     axios.post('/api/users/register', newUSer)
         .then(res => console.log(res.data))
-        .catch(err => console.log(err.response.data));
+        .catch(err => this.setState({ errors: err.response.data }));
 
     
 
   }
 
   render() {
+    const {errors} = this.state;
+
+
+
     return (
       <div className="register">
         <div className="container">
@@ -52,53 +57,70 @@ class Register extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
               <p className="lead text-center">Create your DevConnector account</p>
-              <form onSubmit={this.onSubmit}>
+              <form noValidate onSubmit={this.onSubmit}>
+
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.name
+                    })}
                     placeholder="Name"
                     name="name"
                     value={this.state.value}
                     onChange={this.changeValue}
-                    
                   />
+                  {errors.name && (<div className="invalid-feedback"> {errors.name} </div>)}
                 </div>
+
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid' : errors.email
+                    })}
                     placeholder="Email Address"
                     name="email"
                     value={this.state.email}
                     onChange={this.changeValue}
                   />
+                  {errors.email && (<div className="invalid-feedback"> {errors.email} </div>)}
                   <small className="form-text text-muted">
                     This site uses Gravatar so if you want a profile image, use
                     a Gravatar email
                   </small>
                 </div>
+
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid' : errors.password
+                    })}
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.changeValue}
                   />
+                  {errors.password && (<div className="invalid-feedback"> {errors.password} </div>)}
                 </div>
+
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid' : errors.password2
+                    })}
                     placeholder="Confirm Password"
                     name="password2"
                     value={this.state.password2}
                     onChange={this.changeValue}
                   />
+                  {errors.password2 && (<div className="invalid-feedback"> {errors.password2} </div>)}
                 </div>
+
                 <input type="submit" className="btn btn-info btn-block mt-4" />
+
               </form>
             </div>
           </div>
