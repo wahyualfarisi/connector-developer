c41ba1c02220981addfd6426774db3540f42beba
wahyu alfarisi <wahyualfarisi@gmail.com>
2019-04-23 10:57:06 +0700

    add change package.json, basic layout

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
