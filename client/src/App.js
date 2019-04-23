f29c1484fed653bad938a7f726e07917cfb88565
wahyu alfarisi <wahyualfarisi@gmail.com>
2019-04-23 14:45:28 +0700

    install axios , completed from login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router , Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact={true} path="/" component={ Landing } title={'HEllo'} />
+          <div className="container" >
+            <Route exact path="/register" component={ Register }  />
+            <Route exact path="/login" component={ Login } />
+          </div>
+      </div>
+    </Router>
   );
 }
 
